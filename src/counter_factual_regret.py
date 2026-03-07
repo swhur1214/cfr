892079880ef821590878b9c585212d01cd1f6d1a
Seongwoo Hur <swhur1214@gmail.com>
2026-03-06 19:16:00 -0600

    remove CFRTrainer wrapper

--- src/counter_factual_regret.py
+++ src/counter_factual_regret.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from regret_matching import RegretMatching
-from util import get_nodes_top_down, linear_utility
+from util import get_nodes_top_down
 
 
 class CounterFactualRegret:
@@ -89,79 +89,3 @@ class CounterFactualRegret:
                 x_bar[(j, a)] = parent_prob * local_average[j][idx]
 
         return x_bar
-
-
-class CounterFactualRegretTrainer:
-    """CFRM trainer for 2-player zero-sum extensive_form_games.
-    One CFRM object for each player.
-    """
-
-    def __init__(self, efg: dict, tfsdp0: dict, tfsdp1: dict, plus: bool = False):
-        """
-
-        Args:
-            efg: dict
-                EFG representation of the game. Used for utility computation.
-            tfsdp0: dict
-                TFSDP representation for player 0. Used for CFRM training.
-            tfsdp1: dict
-                TFSDP representation for player 1. Used for CFRM training.
-            plus: bool
-                Whether to use regret matching+ (RM+) or vanilla regret matching (RM).
-        """
-        self._efg = efg
-        self._tfsdp0 = tfsdp0
-        self._tfsdp1 = tfsdp1
-
-        self._cfr0 = CounterFactualRegret(self._tfsdp0, plus)
-        self._cfr1 = CounterFactualRegret(self._tfsdp1, plus)
-
-    def _compute_utility(self, x0: dict, x1: dict) -> tuple[dict, dict]:
-        """
-        Compute linear utility for each player given their strategies.
-
-        Args:
-            x0: dict[sequence -> [0, 1]]
-                strategy of player 0. (sequence-form)
-            x1: dict[sequence -> [0, 1]]
-                strategy of player 1. (sequence-form)
-
-        Returns:
-            l0: dict[sequence -> float]
-                linear utility for player 0 for each sequence.
-            l1: dict[sequence -> float]
-                linear utility for player 1 for each sequence.
-        """
-        l0 = linear_utility(self._efg, self._tfsdp0, player=0, opponent_x=x1)
-        l1 = linear_utility(self._efg, self._tfsdp1, player=1, opponent_x=x0)
-        return l0, l1
-
-    def step(self):
-        """One iteration of CFRM training.
-
-        Returns:
-            x_bar_0: dict[sequence -> [0, 1]]
-                final trained strategy of player 0. (sequence-form)
-            x_bar_1: dict[sequence -> [0, 1]]
-                final trained strategy of player 1. (sequence-form)
-        """
-        x0 = self._cfr0.next_strategy()
-        x1 = self._cfr1.next_strategy()
-
-        l0, l1 = self._compute_utility(x0, x1)
-
-        self._cfr0.observe_utility(l0)
-        self._cfr1.observe_utility(l1)
-    
-    def average_strategy(self) -> tuple[dict, dict]:
-        """Compute the average strategy for both players. Only called at the end of training.
-
-        Returns:
-            x_bar_0: dict[sequence -> [0, 1]]
-                final average strategy of player 0. (sequence-form)
-            x_bar_1: dict[sequence -> [0, 1]]
-                final average strategy of player 1. (sequence-form)
-        """
-        x_bar_0 = self._cfr0.average_strategy()
-        x_bar_1 = self._cfr1.average_strategy()
-        return x_bar_0, x_bar_1
